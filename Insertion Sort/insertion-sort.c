2af7b979f0b17295a9f557c18777838ef4be9f02
Can Sürmeli <c_surmeli@icloud.com>
2019-11-11 10:07:06 +0100

    Finished implementing the insertion sort.
    There is room for improvement but it functions as expected.

--- Insertion Sort/insertion-sort.c
+++ Insertion Sort/insertion-sort.c
@@ -1,6 +1,31 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
+int * retrieveNumbersFromFileIntoArray(char fileName[]) {
+	FILE *file = fopen(fileName, "r");
+	char *fileLine = NULL;
+	size_t len = 0;
+	ssize_t read;
+	static int numbers[100000];
+	int numberIndex= 0;
+
+	// check if the file exists
+	if (file == NULL) {
+		printf("Could not open the file!\n");
+		return 0;
+	}
+
+	// retieve the file contents
+	do {
+		read = getline(&fileLine, &len, file);
+		numbers[numberIndex] = atoi(fileLine);
+		numberIndex++;
+	} while(read != -1);
+
+	return numbers;
+}
 
 void insertionSort(int array[], int n) {
 	int i, key, j;
@@ -22,25 +47,32 @@ void printArray(int array[], int n) {
 	int i;
 
 	for (i=0; i < n; i++) {
-		printf("%d ", array[i]);
+		printf("%d\n", array[i]);
 	}
 }
 
-int main() {
-	int array[] = { 12, 11, 13, 5, 6 };
-	int n = sizeof(array) / sizeof(array[0]);
+int main(int argc, char* argv[]) {
+	char *fileName;
+	int *numbers;
+	int numberCount;
 	clock_t start, end;
 	float secondsElapsed;
 
+	// parse the command line arguments
+	fileName = argv[1];
+	numberCount = atoi(argv[2]);
+
+	numbers = retrieveNumbersFromFileIntoArray(fileName);
+
 	start = clock();
-	insertionSort(array, n);
+	insertionSort(numbers, numberCount);
 	end = clock();
 
 	secondsElapsed = (float)(end - start) / CLOCKS_PER_SEC;
 	printf("TIME ELAPSED: %.6f\n", secondsElapsed);
 
-	printf("SORTED ARRAY:\n");
-	printArray(array, n);
+	/*printf("SORTED ARRAY:\n");*/
+	/*printArray(numbers, numberCount);*/
 
 	return 0;
 }
