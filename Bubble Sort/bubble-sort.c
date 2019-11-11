eea5f6cd90f81870d693b32e5f609d64d3f7bcd0
Can Sürmeli <c_surmeli@icloud.com>
2019-11-11 09:38:04 +0100

    Finished the bubble sort implementation.
    For sure, there still is room for improvement but it functions as
    expected none at the least.

--- Bubble Sort/bubble-sort.c
+++ Bubble Sort/bubble-sort.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 
-#define LSIZ 128
-#define RSIZ 10
+int * retrieveNumbersFromFileIntoArray(char fileName[]) {
+	FILE *file = fopen(fileName, "r");
+	char *fileLine = NULL;
+	size_t len = 0;
+	ssize_t read;
+	static int numbers[100000];
+	int numberIndex= 0;
 
-void retrieveContentsFromFileIntoArray(char fileName[]) {
-	char lineContent[RSIZ][LSIZ];
-	int i = 0;
-	FILE *numbersFile = fopen(fileName, "r");
-
-	printf("starting reading");
-
-	while(fgets(lineContent[i], LSIZ, numbersFile)) {
-		lineContent[i][strlen(lineContent[i]) - 1] = '\0';
-		i++;
+	// check if the file exists
+	if (file == NULL) {
+		printf("Could not open the file!\n");
+		return 0;
 	}
+
+	// retieve the file contents
+	do {
+		read = getline(&fileLine, &len, file);
+		numbers[numberIndex] = atoi(fileLine);
+		numberIndex++;
+	} while(read != -1);
+
+	return numbers;
 }
 
 void swap(int *a, int *b) {
@@ -40,27 +49,35 @@ void printArray(int array[], int n) {
 	int i;
 
 	for (i=0; i < n; i++) {
-		printf("%d ", array[i]);
+		printf("%d\n", array[i]);
 	}
 }
 
 int main(int argc, char* argv[]) {
-	/*retrieveContentsFromFileIntoArray("test.txt");*/
-
-	int array[] = {64, 34, 25, 12, 22, 11, 90};
-	int n = sizeof(array) / sizeof(array[0]);
+	char *fileName;
+	int *numbers;
+	int numberCount;
 	clock_t start, end;
 	float secondsElapsed;
 
+	// parse the command line arguments
+	fileName = argv[1];
+	numberCount = atoi(argv[2]);
+
+	numbers = retrieveNumbersFromFileIntoArray(fileName);
+
+	/*int n = sizeof(&numbers) / sizeof(numbers[0]);*/
+	/*printf("%d", n);*/
+
 	start = clock();
-	bubbleSort(array, n);
+	bubbleSort(numbers, numberCount);
 	end = clock();
 
 	secondsElapsed = (float)(end - start) / CLOCKS_PER_SEC;
 	printf("TIME ELAPSED: %.6f\n", secondsElapsed);
 
-	printf("SORTED ARRAY:\n");
-	printArray(array, n);
+	/*printf("SORTED ARRAY:\n");*/
+	/*printArray(numbers, numberCount);*/
 
 	return 0;
 }
