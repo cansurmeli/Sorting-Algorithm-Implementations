2df4dc3839058e342de8c5b405d3cdeb9096f2aa
Can Sürmeli <c_surmeli@icloud.com>
2019-11-17 15:05:04 +0100

    Finished implementing the insertion sort.

--- Insertion Sort/insertion-sort.c
+++ Insertion Sort/insertion-sort.c
@@ -1,34 +1,16 @@
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include "../shared-code-base/c/generateCSVFile.c"
+#include "../shared-code-base/c/printArray.c"
+#include "../shared-code-base/c/retrieveNumbersFromFileIntoArray.c"
 
-int * retrieveNumbersFromFileIntoArray(char fileName[]) {
-	FILE *file = fopen(fileName, "r");
-	char *fileLine = NULL;
-	size_t len = 0;
-	ssize_t read;
-	static int numbers[100000];
-	int numberIndex= 0;
-
-	// check if the file exists
-	if (file == NULL) {
-		printf("Could not open the file!\n");
-		return 0;
-	}
-
-	// retieve the file contents
-	do {
-		read = getline(&fileLine, &len, file);
-		numbers[numberIndex] = atoi(fileLine);
-		numberIndex++;
-	} while(read != -1);
-
-	return numbers;
-}
-
-void insertionSort(int array[], int n) {
+void insertionSort(int array[], int n, int *operationCount) {
 	int i, key, j;
+
+	printf("Started with the insertion sort.\n");
 
 	for ( i = 1; i < n; i++) {
 		key = array[i];
@@ -37,39 +19,41 @@ void insertionSort(int array[], int n) {
 		while (j >= 0 && array[j] > key) {
 			array[j + 1] = array[j];
 			j = j - 1;
+			(*operationCount)++;
 		}
 
 		array[j + 1] = key;
 	}
-}
-
-void printArray(int array[], int n) {
-	int i;
-
-	for (i=0; i < n; i++) {
-		printf("%d\n", array[i]);
-	}
+	
+	printf("Ended with the insertion sort.\n");
 }
 
 int main(int argc, char* argv[]) {
 	char *fileName;
-	int *numbers;
-	int numberCount;
+	int numberCount,
+			operationCount = 0;
 	clock_t start, end;
 	float secondsElapsed;
 
-	// parse the command line arguments
+	// retrieve the file name from the command line
 	fileName = argv[1];
+
+	// retrieve the number count from the command line
 	numberCount = atoi(argv[2]);
 
-	numbers = retrieveNumbersFromFileIntoArray(fileName);
+	// Generate an array based on the given number count
+	int numbers[numberCount];
+
+	retrieveNumbersFromFileIntoArray(fileName, numbers);
 
 	start = clock();
-	insertionSort(numbers, numberCount);
+	insertionSort(numbers, numberCount, &operationCount);
 	end = clock();
 
 	secondsElapsed = (float)(end - start) / CLOCKS_PER_SEC;
 	printf("TIME ELAPSED: %.6f\n", secondsElapsed);
+
+	generateCSVFile(fileName, secondsElapsed, numberCount, operationCount);
 
 	/*printf("SORTED ARRAY:\n");*/
 	/*printArray(numbers, numberCount);*/
